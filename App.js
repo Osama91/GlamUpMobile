7b86c5eae950b262e56e595c8819257a3c36df67
osamaGamal <Master_Wolferien@hotmail.com>
2019-03-27 01:08:26 +0200

    Video

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import YouTube from 'react-native-youtube'
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -15,8 +15,19 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>GlamUp</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <YouTube
+  videoId="KVZ-P-ZI6W4"   // The YouTube video ID
+  play={true}             // control playback of video with true/false
+  fullscreen={true}       // control whether the video should play in fullscreen or inline
+  loop={true}             // control whether the video should loop when ended
+ 
+  onReady={e => this.setState({ isReady: true })}
+  onChangeState={e => this.setState({ status: e.state })}
+  onChangeQuality={e => this.setState({ quality: e.quality })}
+  onError={e => this.setState({ error: e.error })}
+ 
+  style={{ alignSelf: 'stretch', height: 300 }}
+/>
       </View>
     );
   }
