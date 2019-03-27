35cb354ca8860a807b9837de90460f850292d17d
osamaGamal <Master_Wolferien@hotmail.com>
2019-03-27 23:42:05 +0200

    Video Fixed

--- App.js
+++ App.js
@@ -6,9 +6,10 @@ import YouTube from 'react-native-youtube'
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>GlamUp</Text>
-        <YouTube
+      <View style={styles.container}>        
+        <YouTube resizeMode="cover"
+        controls={0}
+        autoplay={true}
   videoId="KVZ-P-ZI6W4"   // The YouTube video ID
   play={true}             // control playback of video with true/false
   //fullscreen={true}       // control whether the video should play in fullscreen or inline
@@ -17,10 +18,11 @@ export default class App extends Component {
   onReady={() => this.setState({ isReady: true })}
   onChangeState={e => this.setState({ status: e.state })}
   onChangeQuality={e => this.setState({ quality: e.quality })}
-  onError={e => this.setState({ error: e.error })}
- 
+  onError={e => this.setState({ error: e.error })} 
   style={styles.backgroundVideo}
 />
+<Text style={styles.welcome}>GlamUp</Text>
+<Text style={styles.welcome}>Login</Text>
       </View>
     );
   }
@@ -37,6 +39,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    color:'#ffffff',
   },
   instructions: {
     textAlign: 'center',
