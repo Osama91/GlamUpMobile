e4963237fa5b12ac4f5d86accc7226fc59abe36c
osamaGamal <Master_Wolferien@hotmail.com>
2019-04-14 00:25:10 +0200

    fb

--- Components/SignUpComponent.js
+++ Components/SignUpComponent.js
@@ -121,26 +121,7 @@ class SignUp extends Component {
           button title="Continue With Facebook"
           onPress={this.handleFacebookLogin}
           type='facebook'
-        />
-
-        {/* <LoginButton 
-          onLoginFinished={
-            (error, result) => {
-              if (error) {
-                console.log("login has error: " + result.error);
-              } else if (result.isCancelled) {
-                console.log("login is cancelled.");
-              } else {
-                AccessToken.getCurrentAccessToken().then(
-                  (data) => {
-                    console.log(data.accessToken.toString())
-                  }
-                )
-              }
-            }
-          }
-          onLogoutFinished={() => console.log("logout.")} /> */}
-     
+        />     
 
         <SocialIcon style={{ marginBottom: 50 }} onPress={()=> this.instagramLogin.show()}
           button title="Sign Up With instagram"
